5eff6eedbefadef5cf0d9e89ccbeb908766d2184
Greg Lawrence <gregaudio@gmail.com>
2018-01-08 11:57:57 -0600

    about halfway through exercises, lunch time

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace linq
 {
@@ -6,7 +8,114 @@ namespace linq
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Find the words in the collection that start with the letter 'L'
+            List<string> fruits = new List<string>() { "Lemon", "Apple", "Orange", "Lime", "Watermelon", "Loganberry" };
+
+            IEnumerable<string> LFruits = from fruit in fruits
+                                          where fruit.StartsWith("L")
+                                          select fruit;
+
+            foreach (string fruit in LFruits)
+            {
+                Console.Write(fruit + ", ");
+            }
+
+
+            Console.WriteLine();
+
+            // Which of the following numbers are multiples of 4 or 6
+            List<int> numbers = new List<int>()
+            {
+                15, 8, 21, 24, 32, 13, 30, 12, 7, 54, 48, 4, 49, 96
+            };
+
+            IEnumerable<int> fourSixMultiples = numbers.Where(n => n % 4 == 0 || n % 6 == 0);
+
+
+            foreach (int num in fourSixMultiples)
+            {
+                Console.WriteLine(num);
+            }
+
+
+            Console.WriteLine();
+
+            // Order these student names alphabetically, in descending order (Z to A)
+            List<string> names = new List<string>()
+            {
+                "Heather", "James", "Xavier", "Michelle", "Brian", "Nina",
+                "Kathleen", "Sophia", "Amir", "Douglas", "Zarley", "Beatrice",
+                "Theodora", "William", "Svetlana", "Charisse", "Yolanda",
+                "Gregorio", "Jean-Paul", "Evangelina", "Viktor", "Jacqueline",
+                "Francisco", "Tre"
+            };
+
+            IEnumerable<string> descend = from name in names
+                                          orderby name[0] descending
+                                          select name;
+
+            foreach (string name in descend)
+            {
+                Console.Write(name + ", ");
+            }
+
+
+            Console.WriteLine();
+
+            // Build a collection of these numbers sorted in ascending order
+            List<int> moreNumbers = new List<int>()
+            {
+                15, 8, 21, 24, 32, 13, 30, 12, 7, 54, 48, 4, 49, 96
+            };
+
+            Console.WriteLine();
+
+            IEnumerable<int> ascendingNumbers = from num in moreNumbers
+                                                orderby num ascending
+                                                select num;
+
+            foreach (int num in ascendingNumbers)
+            {
+                Console.Write(num + ", ");
+            }
+
+            Console.WriteLine();
+
+            // Output how many numbers are in this list
+            List<int> andMoreNumbers = new List<int>()
+            {
+                15, 8, 21, 24, 32, 13, 30, 12, 7, 54, 48, 4, 49, 96
+            };
+
+            Console.WriteLine(andMoreNumbers.Count);
+
+
+            Console.WriteLine();
+
+            // How much money have we made?
+            List<double> purchases = new List<double>()
+            {
+                2340.29, 745.31, 21.76, 34.03, 4786.45, 879.45, 9442.85, 2454.63, 45.65
+            };
+
+            double totalPurchases = purchases.Sum();
+
+            Console.WriteLine($"The total amount is ${totalPurchases}");
+        
+        
+            Console.WriteLine();
+
+
+            // What is our most expensive product?
+            List<double> prices = new List<double>()
+            {
+                879.45, 9442.85, 2454.63, 45.65, 2340.29, 34.03, 4786.45, 745.31, 21.76
+            };
+
+            double mostExpensiveItem = prices.Max();
+
+            Console.WriteLine($"The most expensive item is ${mostExpensiveItem}");
+
         }
     }
 }
